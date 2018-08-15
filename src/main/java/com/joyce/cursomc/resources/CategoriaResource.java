1ff6bb7fc34834855a1ae9bb8376430cec2e9aae
joyceambrosio <joyceambrosio@users.noreply.github.com>
2018-08-15 12:23:47 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/joyce/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/joyce/cursomc/resources/CategoriaResource.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -35,15 +37,17 @@ public class CategoriaResource {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Categoria> insert(@RequestBody Categoria obj) {
+	public ResponseEntity<Categoria> insert(@Valid @RequestBody CategoriaDTO objDTO) {
+		Categoria obj = service.fromDTO(objDTO);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Categoria> update(@RequestBody Categoria obj, @PathVariable Integer id)
+	public ResponseEntity<Categoria> update(@Valid @RequestBody CategoriaDTO objDTO, @PathVariable Integer id)
 			throws ObjectNotFoundException {
+		Categoria obj = service.fromDTO(objDTO);
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
--- src/main/java/com/joyce/cursomc/services/CategoriaService.java
+++ src/main/java/com/joyce/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.joyce.cursomc.domain.Categoria;
+import com.joyce.cursomc.dto.CategoriaDTO;
 import com.joyce.cursomc.repositories.CategoriaRepository;
 import com.joyce.cursomc.services.exceptions.DataIntegrityException;
 
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }
